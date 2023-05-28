78e64bb14edd93d010541f163d8bb1274fedd9f7
Gregory John Casamento <greg.casamento@gmail.com>
2023-05-27 23:10:29 -0400

    Add all current code

--- NSUserActivity_test/AppDelegate.m
+++ NSUserActivity_test/AppDelegate.m
@@ -14,8 +14,23 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
   // Insert code here to initialize your application
+  NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType: @"INStartAudioCallIntent"];
+
+  activity.title = @"title";
+  activity.delegate = self;
+  activity.needsSave = YES;
+  activity.webpageURL = [NSURL URLWithString:@"https://www.google.com"];
+  activity.userInfo = @{@"name": @"test", @"deep_link": @"test"};
+  // set requiredUserInfoKeys if you're also setting webPageURL !!
+  // otherwise, userInfo will be nil !!
+  activity.requiredUserInfoKeys = [NSSet setWithArray:@[@"name", @"deep_link"]];
+  activity.eligibleForSearch = YES;
+  activity.eligibleForPublicIndexing = YES;
+
+  [activity becomeCurrent];
 }
 
 
@@ -28,5 +43,33 @@
   return YES;
 }
 
+- (BOOL)application:(NSApplication *)application willContinueUserActivityWithType:(NSString *)userActivityType
+{
+  return YES;
+}
 
+- (BOOL)application:(NSApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<NSUserActivityRestoring>> * _Nonnull))restorationHandler
+{
+  NSLog(@" Userinfo Data==>%@  useractivityTitle ==>%@  activityType-->%@ webpage-->%@",userActivity.userInfo , userActivity.title,userActivity.activityType,userActivity.webpageURL );
+  return YES;
+}
+
+- (void)application:(NSApplication *)application didUpdateUserActivity:(NSUserActivity *)userActivity
+{
+  NSLog(@"didUpdateUserActivity");
+}
+
+// Save activity...
+
+- (void)userActivityWillSave:(NSUserActivity *)userActivity
+{
+  userActivity.userInfo = @{@"name": @"test", @"deep_link": @"test"};
+  NSLog(@"userActivityWillSave");
+}
+
+- (void)updateUserActivityState:(NSUserActivity *)userActivity
+{
+  NSLog(@"updateUserActivityState");
+  [userActivity addUserInfoEntriesFromDictionary:@{@"name": @"test", @"deep_link": @"test"}];
+}
 @end
