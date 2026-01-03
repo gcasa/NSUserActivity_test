005cd8b1746f9279307e6ce1d74a719c3eb7bd25
Gregory Casamento <greg.casamento@gmail.com>
2026-01-03 05:02:03 -0500

    Initial Commit

---
+++ NSUserActivity_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSUserActivity_test
+//
+//  Created by Gregory Casamento on 1/3/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
